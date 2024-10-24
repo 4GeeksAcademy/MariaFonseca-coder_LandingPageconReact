3b0dfe7a211797f50c3adb7147c10c18c2a3becb
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-24 00:15:41 +0000

    Primer componente listo

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,41 +1,63 @@
 import React from "react";
 
+// Componente NavbarBrand
+const NavbarBrand = () => (
+	<a className="navbar-brand" href="#">Navbar</a>
+);
+
+// Componente NavbarToggler
+const NavbarToggler = () => (
+	<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span className="navbar-toggler-icon"></span>
+	</button>
+);
+
+// Componente NavItem
+const NavItem = ({ href, children, active, disabled }) => (
+	<li className="nav-item">
+		<a className={`nav-link ${active ? 'active' : ''} ${disabled ? 'disabled' : ''}`} href={href}>{children}</a>
+	</li>
+);
+
+// Componente Dropdown
+const Dropdown = () => (
+	<li className="nav-item dropdown">
+		<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			Dropdown
+		</a>
+		<ul className="dropdown-menu">
+			<li><a className="dropdown-item" href="#">Action</a></li>
+			<li><a className="dropdown-item" href="#">Another action</a></li>
+			<li><hr className="dropdown-divider" /></li>
+			<li><a className="dropdown-item" href="#">Something else here</a></li>
+		</ul>
+	</li>
+);
+
+// Componente NavbarSearch
+const NavbarSearch = () => (
+	<form className="d-flex" role="search">
+		<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+		<button className="btn btn-outline-success" type="submit">Search</button>
+	</form>
+);
+
+// Componente principal Navbar
 const Navbar = () => {
 	return (
 		<div>
 			<nav className="navbar navbar-expand-lg bg-light">
 				<div className="container-fluid">
-					<a className="navbar-brand" href="#">Navbar</a>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
+					<NavbarBrand />
+					<NavbarToggler />
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a className="nav-link active" aria-current="page" href="#">Home</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Link</a>
-							</li>
-							<li className="nav-item dropdown">
-								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Dropdown
-								</a>
-								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="#">Action</a></li>
-									<li><a className="dropdown-item" href="#">Another action</a></li>
-									<li><hr className="dropdown-divider"></li>
-									<li><a className="dropdown-item" href="#">Something else here</a></li>
-								</ul>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link disabled">Disabled</a>
-							</li>
+							<NavItem href="#" active={true}>Home</NavItem>
+							<NavItem href="#">Link</NavItem>
+							<Dropdown />
+							<NavItem href="#" disabled={true}>Disabled</NavItem>
 						</ul>
-						<form className="d-flex" role="search">
-							<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-								<button className="btn btn-outline-success" type="submit">Search</button>
-						</form>
+						<NavbarSearch />
 					</div>
 				</div>
 			</nav>
