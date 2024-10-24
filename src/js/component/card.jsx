bf8784bfc9c5af35ecbcc6812de6d35cecd84240
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-24 02:16:57 +0000

    avance 2

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+    const { image, title, description, footer } = props.cards
     return (
-        <div className="card" style={{ width: '18rem' }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        <div className="card-group">
+            <div className="card">
+                <img src={image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
+                <div className="card-footer">
+                    <button>{footer}</button>
+                </div>
             </div>
         </div>
     );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,13 +3,36 @@ import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 import Card from "./card.jsx";
 
+const cards = [{
+	image: "https://picsum.photos/id/10/200/300",
+	title: "Parque",
+	description: "Parque con lindo paisaje",
+},
+{
+	image: "https://picsum.photos/id/17/200/300",
+	title: "Sendero",
+	description: "Sendero con lindo paisaje",
+},
+{
+	image: "https://picsum.photos/id/28/200/300",
+	title: "Bosque",
+	description: "Bosque con lindo paisaje",
+},
+{
+	image: "https://picsum.photos/id/37/200/300",
+	title: "Costa",
+	description: "Costa con lindo paisaje",
+}]
+
 
 const Home = () => {
 	return (
 		<div className="text-center">
 			<Navbar />
 			<Jumbotron />
-			<Card />
+			<div>
+				{cards.map((item, index) => (<Card key={index} cards={item} />))}
+			</div>
 		</div >
 	);
 };
