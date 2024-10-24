7ce7180e763dc60322da9f15cb1192de2c254d96
María José Fonseca Castro <mfonsecac052@gmail.com>
2024-10-24 00:28:06 +0000

    tercer componente listo, solo falta diseño y cantidad de cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+
 
 const Home = () => {
 	return (
 		<div className="text-center">
 			<Navbar />
 			<Jumbotron />
-		</div>
+			<Card />
+		</div >
 	);
 };
 
